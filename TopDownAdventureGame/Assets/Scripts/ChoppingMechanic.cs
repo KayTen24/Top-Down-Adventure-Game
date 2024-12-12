593beeff380937dc167b075519d65fb13e8b45e2
KayTen24 <kay-ten@hotmail.com>
2024-12-12 13:31:12 -0500

    Changed 9-sliced

--- TopDownAdventureGame/Assets/Scripts/ChoppingMechanic.cs
+++ TopDownAdventureGame/Assets/Scripts/ChoppingMechanic.cs
@@ -16,12 +16,19 @@ public class ChoppingMechanic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.z = transform.position.z;
+            //target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            /target.z = transform.position.z;
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, target,speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target,speed * Time.deltaTime);*/
+    }
+
+    void OnMouseDown()
+    {
+        target.x += 1f;
+        Debug.Log("clicked the cucumber!");
+        transform.position = target;
     }
 }
